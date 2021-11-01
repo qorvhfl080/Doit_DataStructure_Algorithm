e5c27609daf98e59d7dd876593d42307e6684485
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-11-01 12:54:07 +0900

    스택 예제

---
+++ src/main/kotlin/stack/IntStackTest.kt
@@ -0,0 +1,34 @@
+package stack
+
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val s = Stack<Int>()
+
+    while (true) {
+        println("현재 데이터수: ${s.size}/${s.capacity()}")
+        print("(1)푸시 (2)팝 (3)피크 (4)덤프 (0)종료: ")
+        val menu = sc.nextInt()
+        if (menu == 0)
+            break
+
+        var x: Int
+        when (menu) {
+            1 -> {
+                print("데이터: ")
+                x = sc.nextInt()
+                s.push(x)
+            }
+            2 -> {
+                x = s.pop()
+                println("팝한 데이터: $x")
+            }
+            3 -> {
+                x = s.peek()
+                println("피크한 데이터: $x")
+            }
+            4 -> println(s.toString())
+        }
+    }
+}
